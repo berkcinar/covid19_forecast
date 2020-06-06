407aaaaab1f7e81dde8e9902a1ae64d3cea6dea0
berkcinar <berkcinar01@gmail.com>
2020-06-07 02:46:14 +0300

    Class added

--- covid19ML.py
+++ covid19ML.py
@@ -2,20 +2,19 @@ import requests
 import pandas as pd
 from datetime import datetime as dt
 
-def get_daily_values():
-    end_point = 'https://api.covid19api.com/dayone/country/turkey/status/confirmed'
-    daily_confirmed = requests.get(end_point)
-    daily_confirmed_json=daily_confirmed.json()
+class Covid19():
+    def get_daily_values(self):
+        end_point = 'https://api.covid19api.com/dayone/country/turkey/status/confirmed'
+        daily_confirmed = requests.get(end_point)
+        daily_confirmed_json=daily_confirmed.json()
+        country_df = pd.DataFrame()
+        for i in daily_confirmed_json:
+            country_df = country_df.append(
+                {
+                    'timeslot': (dt.strptime(str(i.get('Date')), "%Y-%m-%dT%H:%M:%SZ")).strftime("%Y-%m-%d"),
+                    'daily_total_case': i.get('Cases'),
+                }, ignore_index=True)
 
-    print(daily_confirmed_json)
+    def run(self):
+        self.get_daily_values()
 
-    country_df = pd.DataFrame()
-    for i in daily_confirmed_json:
-        country_df = country_df.append(
-            {
-                'timeslot': (dt.strptime(str(i.get('Date')), "%Y-%m-%dT%H:%M:%SZ")).strftime("%Y-%m-%d"),
-                'daily_total_case': i.get('Cases'),
-            }, ignore_index=True)
-
-    print(country_df)
-get_daily_values()
